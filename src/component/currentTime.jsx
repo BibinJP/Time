0993af2f7ab094fe62d0ea9bf86b855d40832649
BibinJP <97421150+BibinJP@users.noreply.github.com>
2022-01-28 17:41:23 +0530

    new

---
+++ src/component/currentTime.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Time extends Component {
+    state={
+        date:new Date()
+    };
+    timer(){
+        setInterval(() => {
+            this.setState({date:new Date()})
+            
+        }, 1000);
+      
+    }
+    render() { 
+        return( 
+        <div>
+            <h1>Current Time</h1>
+            <h2> {this.state.date.toLocaleTimeString()}</h2>
+            {this.timer()}             
+        </div>
+        );
+    }
+}
+ 
+export default Time;
